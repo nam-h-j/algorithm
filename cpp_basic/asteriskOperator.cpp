0ff1f3a61346cb365bac0fb336746aebb83f75c1
nam <hzzn74@gmail.com>
2022-04-12 10:20:35 +0900

    pointer, dereference operator

---
+++ cpp_basic/asteriskOperator.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // &로 메모리 주소 가져오기
+    string a = "abcda";//할당
+    cout << "a의 값(a) :"<< a << "\n";
+    cout << "a의 주소값(&a) :" << &a << "\n";
+}
