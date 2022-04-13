1bf11e089eab40d546ec1a7fcf0ff2cfb0de70f8
nam <hzzn74@gmail.com>
2022-04-13 12:26:11 +0900

    iterator

--- cpp_basic/asteriskOperator.cpp
+++ cpp_basic/asteriskOperator.cpp
@@ -5,8 +5,8 @@ int main(){
     string a = "abcda";//할당
     cout << "a의 값(a) :"<< a << "\n";
     cout << "a의 주소값(&a) :" << &a << "\n";
-    int var_num = 10;// 변수를 하나 선언, 할당 한다
-    int *pointer_var;// 포인터 변수를 하나 선언한다
-    pointer_var = &var_num;// 포인터 변수에 위 변수의 메모리 주소를 할당한다
-    cout << *pointer_var;// 포인터 변수를 역참조연산자를 통해서 값을 취득한다
+
+    string *b = &a;
+    cout << "포인터 변수 b :" << b << "\n";
+    cout << "역참조 연산자 *b : "<< *b << "\n";
 }
